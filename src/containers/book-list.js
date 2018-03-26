ba2a0cca96e99271ee8bcd560c70e7258735295d
Alonsop <alonsop@uakami.com>
2018-03-26 12:16:11 -0400

    Adding BookList container to the project

---
+++ src/containers/book-list.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class BookList extends Component {
+
+  renderlist() {
+    return this.props.books.map((book) => {
+      return (
+        <li
+          key={book.title}
+          className="list-group-item">
+            {book.title}
+        </li>
+      )
+    })
+  }
+
+  render () {
+    return (
+      <ul className="list-group col-md-4">
+        {this.renderlist()}
+      </ul>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    books: state.books
+  }
+}
+
+export default connect(mapStateToProps)(BookList);
